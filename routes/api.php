5dcde5704612e66363f999848c46bc1416d23eca
websmarts <iwmaclagan@bigpond.com>
2018-05-21 15:34:22 +1000

    remove closure route from api routes list so route opt will work

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/submit', 'ReceiveEnquiryController@index');
 Route::post('/message_us', 'MessageUsController@index');
