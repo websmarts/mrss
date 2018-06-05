2901c51fb31afed2715548df8c29bed1715e1282
websmarts <iwmaclagan@bigpond.com>
2018-06-05 14:13:06 +1000

    test home page v 4

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,7 +12,7 @@
 <div class="row" style="background: #fff; margin-top:10px;text-align: center">
   <h2>{!! $page->element('section_1_heading')->content !!}</h2>
   <div class="col-sm-12">
-    <div style="text-align: center"><img   src="{!! $page->element('section_1_image')->content !!}" alt="We Bring Icon" width="300" ></div>
+    <img   src="{!! $page->element('section_1_image')->content !!}" alt="We Bring Icon" style="margin: auto;max-width:400px;" class="img-responsive" >
     <div class="louder">{!! $page->element('section_1_blurb')->content !!}</div>
     <a href="{{ route('storage') }}" class="btn btn-default btn-lg" style="margin-top:15px" >How It Works</a>
     <a href="{{ route('cart') }}" class="btn btn-primary btn-lg" style="margin-top:15px" >Price / Book Now &nbsp;&nbsp;<i class="fas fa-mouse-pointer"></i></a>
