5735a6f402fef1673977cd140eefcab106870dad
websmarts <iwmaclagan@bigpond.com>
2018-06-05 12:09:04 +1000

    test home page v 3

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,6 +9,20 @@
 
 {{-- dump($page) --}}
 
+<div class="row" style="background: #fff; margin-top:10px;text-align: center">
+  <h2>{!! $page->element('section_1_heading')->content !!}</h2>
+  <div class="col-sm-12">
+    <div style="text-align: center"><img   src="{!! $page->element('section_1_image')->content !!}" alt="We Bring Icon" width="300" ></div>
+    <div class="louder">{!! $page->element('section_1_blurb')->content !!}</div>
+    <a href="{{ route('storage') }}" class="btn btn-default btn-lg" style="margin-top:15px" >How It Works</a>
+    <a href="{{ route('cart') }}" class="btn btn-primary btn-lg" style="margin-top:15px" >Price / Book Now &nbsp;&nbsp;<i class="fas fa-mouse-pointer"></i></a>
+
+  </div><!-- /.col-lg-12 -->
+</div><!-- /.row -->
+
+
+<div class="greyline"  style="width:80%;margin: auto ; margin-top:50px;"></div>
+
        
 @include('layouts.partials.testimonials')
 
